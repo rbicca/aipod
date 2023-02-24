494d921da9064afc2493a97eb575bf0d376d551b
Ronaldo Melchiades Bicca <ronaldo.bicca@gmail.com>
2023-02-23 22:29:29 -0300

    Loading tracks from SQLite database

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,8 @@
+import { getInitialTracks } from "$lib/server/db";
+import type { PageServerLoad } from "./$types";
+
+export const load = (() => {
+    const tracks = getInitialTracks();
+
+    return { tracks };
+}) satisfies PageServerLoad;
